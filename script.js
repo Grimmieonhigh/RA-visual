691c3229c5513b2aa6d120760d284daec78d252d
Grimmieonhigh <Grimmieonhigh@gmail.com>
2026-06-27 20:05:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Website loaded");
+function openLightbox(img) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
